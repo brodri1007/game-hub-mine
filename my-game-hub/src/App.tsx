27172b8d79f77c9730beed706a45f9cc928a5878
Byron <byronrod@gmail.com>
2024-10-08 10:35:12 -0400

    Add a dynaMIC hEADING

--- my-game-hub/src/App.tsx
+++ my-game-hub/src/App.tsx
@@ -15,6 +15,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -54,22 +55,25 @@ function App() {
           </GridItem>
         </Show>
         <GridItem pl="2" bg="green.300" area="main">
-          <Flex paddingLeft={2} marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector
-                onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
-                }
-                selectedPlatform={gameQuery.platform}
-              />
+          <Box paddingLeft={2}>
+            <GameHeading gameQuery={gameQuery} />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  onSelectPlatform={(platform) =>
+                    setGameQuery({ ...gameQuery, platform })
+                  }
+                  selectedPlatform={gameQuery.platform}
+                />
+              </Box>
               <SortSelector
                 sortOrder={gameQuery.sortOrder}
                 onSelectOrder={(sortOrder) =>
                   setGameQuery({ ...gameQuery, sortOrder })
                 }
               />
-            </Box>
-          </Flex>
+            </Flex>
+          </Box>
           <GameGrid gameQuery={gameQuery} />
         </GridItem>
       </Grid>
